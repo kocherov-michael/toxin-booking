a277d05624ccdc0cb97cb4021d305d257a1d687a
Michael <ko4erov@mail.ru>
2019-08-30 00:28:43 +0300

    fix expanded checkbox script

--- src/js/checkbox.js
+++ src/js/checkbox.js
@@ -7,15 +7,23 @@
     const expandHeaderElement = expandCheckboxElement.querySelector(".expandable-checkbox__header")
     const expandListElement = expandCheckboxElement.querySelector(".expandable-checkbox__list")
     let listHeight = parseFloat(getComputedStyle(expandListElement, null).height.replace("px", ""))
+
+    if (!listHeight) {
+      expandCheckboxElement.classList.add("expandable-checkbox_active")
+      listHeight = parseFloat(getComputedStyle(expandListElement, null).height.replace("px", ""))
+      expandCheckboxElement.classList.remove("expandable-checkbox_active")
+    }
   
     expandHeaderElement.addEventListener('click', function handler() {
       if (expandCheckboxElement.classList.contains("expandable-checkbox_active")) {
         expandListElement.style.height = `${listHeight}px`
+
         const showList = setInterval(() => {
-          console.log(expandListElement.style.height)
           let height = parseInt(expandListElement.style.height.replace("px", ""))
+
           height -= 10
           expandListElement.style.height = `${height}px`
+
           if (height <= 0) {
             expandCheckboxElement.classList.remove("expandable-checkbox_active")
             clearInterval(showList)
@@ -23,11 +31,15 @@
         }, 10)
       } else {
         expandListElement.style.height = "0px"
+
         const showList = setInterval(() => {
           expandCheckboxElement.classList.add("expandable-checkbox_active")
+
           let height = parseInt(expandListElement.style.height.replace("px", ""))
+          
           height += 10
           expandListElement.style.height = `${height}px`
+
           if (height > listHeight) {
             clearInterval(showList)
           }
