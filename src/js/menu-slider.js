d9c9ffc39a7a9636b9ccf29399b667dce814cbc6
Michael <ko4erov@mail.ru>
2019-09-14 18:41:11 +0300

    fix menu slider and fix room card

---
+++ src/js/menu-slider.js
@@ -0,0 +1,65 @@
+function menuSlider () {
+  const filterButtonElement = document.querySelector('[data-filter-button]')
+  const mainButtonElement = document.querySelector('[data-main-button]')
+  const filterButtonWrapperElement = document.querySelector('[data-filter-button-wrapper]')
+  const filterWrapperElement = document.querySelector('[data-filter-wrapper]')
+  const filterTextElement = document.querySelector('[data-filter-text]')
+  const filterCloseElement = document.querySelector('[data-filter-close]')
+  const filterElement = document.querySelector('[data-filter]')
+  const bodyElement = document.querySelector('body')
+
+
+  if (mainButtonElement) {
+    mainButtonElement.addEventListener('click', (event) => {
+      event.stopImmediatePropagation()
+      toggleMenu()
+
+      filterElement.addEventListener('click', (event) => {
+        event.stopImmediatePropagation()
+      })
+    
+      filterButtonWrapperElement.addEventListener('click', (event) => {
+        event.stopImmediatePropagation()
+      })
+    
+      filterTextElement.addEventListener('click', (event) => {
+        event.stopImmediatePropagation()
+      })
+    
+      filterWrapperElement.addEventListener('click', (event) => {
+        event.stopImmediatePropagation()
+        toggleMenu()
+      })
+    
+      filterCloseElement.addEventListener('click', (event) => {
+        event.stopImmediatePropagation()
+        toggleMenu()
+      })
+    })
+
+  }
+
+  if(filterButtonElement) {
+    filterButtonElement.addEventListener('click', (event)=> {
+      event.stopImmediatePropagation()
+      toggleMenu()
+  
+    })
+  }
+
+
+  function toggleMenu () {
+    filterWrapperElement.classList.toggle('search-room__filter-wrapper_show')
+
+    if (bodyElement.style.overflow === 'hidden') {
+      // меню закрывается
+      bodyElement.style.overflow = ''
+
+    } else {
+      // меню открывается
+      bodyElement.style.overflow = 'hidden'
+    }
+  }
+}
+
+export default menuSlider
