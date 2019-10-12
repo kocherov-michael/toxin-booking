7ca5237b5e581afbbc885de396174494935f375a
Michael <ko4erov@mail.ru>
2019-10-12 15:08:26 +0300

    calculator room price ready

--- src/blocks/room-price-card/room-price-card.js
+++ src/blocks/room-price-card/room-price-card.js
@@ -10,67 +10,75 @@ if (document.querySelector('[data-calendar="room-price"]')){
 
 dropdown("#dropdown-guests")
 
-formatPrice('[data-calculator-price]')
+formatPrice('[data-calc-price]')
 
-roomPriceCalculator()
+roomPriceCalculator('[data-calculator-card]')
 
-function roomPriceCalculator () {
-  const arrivalElement = document.querySelector('[data-find-room-arrival="room-price"]')
-  const arrivalInputElement = arrivalElement.querySelector('input')
-  const departureElement = document.querySelector('[data-find-room-departure="room-price"]')
-  const departureInputElement = departureElement.querySelector('input')
-
-  const calendarElement = document.querySelector('[data-calendar="room-price"]')
-  const applyCalendarButton = calendarElement.querySelector('[data-action="apply"]')
-
-  const pricePerDayElement = document.querySelector('[data-room-price-per-day]')
-  const totalElement = document.querySelector('[data-calculator-price="total"]')
-  const allDaysElement = document.querySelector('[data-calculator-price="all-days"]')
-  const daysCountElement = document.querySelector('[data-calculator-price="days-count"]')
-
-
-  applyCalendarButton.addEventListener('click', calculatePrice)
-
-  arrivalInputElement.addEventListener('input', () => {countDays()})
-  departureInputElement.addEventListener('input', () => {countDays()})
-
-  const sourse = {
-    pricePerDay: parseInt(pricePerDayElement.getAttribute('data-room-price-per-day')),
-    days: countDays(),
-    guests: 0,
-    serviceFee: 0,
-    additionalFee: 0,
-    discount: 0
-  }
-  // console.log(parseInt(pricePerDayElement.getAttribute('data-room-price-per-day')))
-
-
-  function countDays() {
+function roomPriceCalculator (selector) {
+  const calculatorCardElements = document.querySelectorAll(selector)
+  for (let i = 0; i < calculatorCardElements.length; i++) {
+    
+    const arrivalElement = calculatorCardElements[i].querySelector('[data-find-room-arrival="room-price"]')
+    const arrivalInputElement = arrivalElement.querySelector('input')
+    const departureElement = calculatorCardElements[i].querySelector('[data-find-room-departure="room-price"]')
+    const departureInputElement = departureElement.querySelector('input')
   
-    const firstDayArr = arrivalInputElement.value.split('.')
-    const lastDayArr = departureInputElement.value.split('.')
-    const firstDay = new Date(firstDayArr[2], firstDayArr[1], firstDayArr[0])
-    const lastDay = new Date(lastDayArr[2], lastDayArr[1], lastDayArr[0])
-
-    return (lastDay.getTime() - firstDay.getTime())/ 86400000
+    setDefaultCalcValues(arrivalInputElement, '19.08.2019')
+    setDefaultCalcValues(departureInputElement, '27.08.2019')
+  
+    const calendarElement = calculatorCardElements[i].querySelector('[data-calendar="room-price"]')
+    const applyCalendarButton = calendarElement.querySelector('[data-action="apply"]')
+  
+    const pricePerDayElement = calculatorCardElements[i].querySelector('[data-room-price-per-day]')
+    const totalElement = calculatorCardElements[i].querySelector('[data-calculator-total]')
+    const allDaysElement = calculatorCardElements[i].querySelector('[data-calculator-all-days]')
+    const daysCountElement = calculatorCardElements[i].querySelector('[data-calculator-days-count]')
+    const discountElement = calculatorCardElements[i].querySelector('[data-calculator-discount]')
+    const feeElement = calculatorCardElements[i].querySelector('[data-calculator-fee]')
+    const addFeeElement = calculatorCardElements[i].querySelector('[data-calculator-add-fee]')
+  
+    const sourse = {
+      pricePerDay: parseInt(pricePerDayElement.getAttribute('data-room-price-per-day')),
+      days: countDays(),
+      guests: 0,
+      serviceFee: 0,
+      additionalFee: 0,
+      discount: 0
+    }
+  
+    calculatePrice ()
+  
+    applyCalendarButton.addEventListener('click', calculatePrice)
+  
+    arrivalInputElement.addEventListener('input', calculatePrice)
+    departureInputElement.addEventListener('input', calculatePrice)
+  
+    function countDays() {
+    
+      const firstDayArr = arrivalInputElement.value.split('.')
+      const lastDayArr = departureInputElement.value.split('.')
+      const firstDay = new Date(firstDayArr[2], firstDayArr[1], firstDayArr[0])
+      const lastDay = new Date(lastDayArr[2], lastDayArr[1], lastDayArr[0])
+  
+      return (lastDay.getTime() - firstDay.getTime())/ 86400000
+    }
+  
+    function calculatePrice () {
+      const days = countDays()
+      daysCountElement.innerHTML = days
+      sourse.days = countDays()
+      let result = sourse.pricePerDay * sourse.days
+  
+      daysCountElement.innerHTML = sourse.days
+      allDaysElement.innerHTML = formatPrice(result)
+  
+      const total = result - parseInt(discountElement.getAttribute('data-calculator-discount')) + parseInt(feeElement.getAttribute('data-calculator-fee')) + parseInt(addFeeElement.getAttribute('data-calculator-add-fee'))
+  
+      totalElement.innerHTML = formatPrice(total)
+    }
   }
+}
 
-  function calculatePrice () {
-    console.log('calc')
-    // const firstDay = arrivalInputElement.value
-    // const lastDay = departureInputElement.value
-    // console.log(parseInt(firstDay))
-    // console.log(lastDay)
-    sourse.days = countDays()
-    let result = sourse.pricePerDay * sourse.days
-
-    daysCountElement.innerHTML = sourse.days
-    allDaysElement.innerHTML = result
-
-
-
-
-
-    totalElement.innerHTML = result
-  }
+function setDefaultCalcValues(element, val) {
+  element.value = val
 }
--- src/js/format-price.js
+++ src/js/format-price.js
@@ -1,8 +1,39 @@
-function formatPrice(selector) {
-  const priceElement = document.querySelectorAll(selector)
+// function formatPrice(selector) {
+//   const priceElement = document.querySelectorAll(selector)
 
-  for (let i = 0; i < priceElement.length; i++) {
-    const price = [...priceElement[i].innerHTML].reverse()
+//   for (let i = 0; i < priceElement.length; i++) {
+//     const price = [...priceElement[i].innerHTML].reverse()
+//     const newPrice = []
+    
+//     const priceArrayWithoutSpace = price.filter(letter => letter !== ' ')
+
+//     for( let i = 0; i < priceArrayWithoutSpace.length; i++) {
+//       if ((Math.floor(i/3) === i/3) && i !== 0) {
+//         newPrice.push(' ')
+//         newPrice.push(priceArrayWithoutSpace[i])
+//       } else {
+//         newPrice.push(priceArrayWithoutSpace[i])
+//       }
+//     }
+//     priceElement[i].innerHTML = `${newPrice.reverse().join('')}`
+//   }
+// }
+
+function formatPrice(selector) {
+  if (typeof(selector) === 'string') {
+
+    const priceElement = document.querySelectorAll(selector)
+  
+    for (let i = 0; i < priceElement.length; i++) {
+      priceElement[i].innerHTML = addSpaces(parseInt(priceElement[i].innerHTML))
+    }
+  }
+  else if (typeof(selector) === 'number') {
+    return addSpaces(selector)
+  }
+
+  function addSpaces ( number) {
+    const price = [...number.toString()].reverse()
     const newPrice = []
     
     const priceArrayWithoutSpace = price.filter(letter => letter !== ' ')
@@ -15,7 +46,7 @@ function formatPrice(selector) {
         newPrice.push(priceArrayWithoutSpace[i])
       }
     }
-    priceElement[i].innerHTML = `${newPrice.reverse().join('')}`
+    return `${newPrice.reverse().join('')}`
   }
 }
 
